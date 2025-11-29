ec634d0247f93b7b43d7e3af43acac1c13a224d1
Jash Gusani <jashgusani78@gmail.com>
2025-11-29 23:54:35 +0530

    [feat]: adding cors system

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import express from "express";
 import { saveVersion, getVersions, deleteHistory } from "./controller.js";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: ["https://audit-frontend-two.vercel.app","http://localhost:5173/"],
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(express.json());
 
 app.post("/save-version", saveVersion);
